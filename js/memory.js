dee05f67381c0ed629b7bfc88abd367fe3658568
Elena Nedkova <lenuza@gmail.com>
2018-04-04 22:23:15 +0300

    Card back added, also stared working on front side

---
+++ js/memory.js
@@ -0,0 +1,14 @@
+/*$(document).ready(function() {
+  $('#one').on('keypress click', function() {
+    $('#one').toggle();
+    $('#purple').toggle();
+  });
+});*/
+
+
+   const cardFlip = document.getElementById("one");
+
+   cardFlip.addEventListener("click", function() {
+
+         cardFlip.classList.add("card1");
+})
