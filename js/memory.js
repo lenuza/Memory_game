4cd832592ad8309ab6c2c2221bb8144bd1954b76
Elena Nedkova <lenuza@gmail.com>
2018-04-09 22:31:06 +0300

    Flip card added - but the file takes long to load

--- js/memory.js
+++ js/memory.js
@@ -1,28 +1,4 @@
 
-/*Shuffle cards*/
-/*cards.Math.floor(Math.random()*16.1);*/
-
-/*let cards = ["fas fa-anchor", "fas fa-binoculars", "fas fa-bell", "fas fa-globe", "fas fa-ship", "fas fa-cloud", "fas fa-coffee", "far fa-life-ring"];
-cards.prototype.shuffle = function() {
-    var input = this;
-
-    for (var i = cards.length-1; i >=0; i--) {
-
-        var randomIndex = Math.floor(Math.random()*(i+1));
-        var itemAtIndex = cards[randomIndex];
-
-        cards[randomIndex] = cards[i];
-        cards[i] = itemAtIndex;
-    }
-    return cards;
-}
-
-var tempArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-tempArray.shuffle();
-
-// and the result is...
-alert(tempArray);*/
-
 /*Assigning card classes*/
 
    const cards = [ "fas fa-anchor",
@@ -42,39 +18,52 @@ alert(tempArray);*/
                    "fas fa-coffee",
                    "far fa-life-ring"];
 
-  /*Another way again stackoverflow
-     i = 0,
-     len = cards.length;
 
-     $('.front').each(function(index, front) {
-       console.log(front);
-       $(front).addClass(cards[i]);
-       ++i;
-       if (i === len) {
-         i = 0;
-       }
-     });*/
+  const frontSides = document.getElementsByClassName("front");
 
-//     $('.front').each(function(index, element) {
-//       $(element).addClass(cards[index % cards.length]);
-//     });
-
-
-
-  const frontSides = document.getElementsByClassName('front');
   for ( let i = 0; i < frontSides.length; i++) {
-    frontSides[i].className += ' ' + cards[i];
+    frontSides[i].className += " " + cards[i];
   }
 
+  /*Shuffle cards*/
+  /*cards.Math.floor(Math.random()*16.1);
 
+  //let cards = ["fas fa-anchor", "fas fa-binoculars", "fas fa-bell", "fas fa-globe", "fas fa-ship", "fas fa-cloud", "fas fa-coffee", "far fa-life-ring"];
+  cards.prototype.shuffle = function() {
+      var input = this;
+
+      for (var i = cards.length-1; i >=0; i--) {
+
+          var randomIndex = Math.floor(Math.random()*(i+1));
+          var itemAtIndex = cards[randomIndex];
+
+          cards[randomIndex] = cards[i];
+          cards[i] = itemAtIndex;
+      }
+      return cards;
+  }
+
+  var tempArray = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+  tempArray.shuffle();
+
+  // and the result is...
+  alert(tempArray);
 
 /*Select cards*/
-/*const cardFlip = document.getElementById("one");
 
-cardFlip.addEventListener("click", function() {
-      document.querySelectorAll(".front").setAttribute("transform", "perspective(500px) rotateY(0deg)");
+   backSides = document.getElementsByClassName("back");
 
-  });*/
+  for ( let i = 0; i < backSides.length; i++) {
+   backSides[i].addEventListener("click", function() {
+   backSides[i].style.transform = "perspective(500px) rotateY(-180deg)";
+   frontSides[i].style.transform = "perspective(500px) rotateY(0deg)";
+ });
+};
+
 /*Making a match*/
+
+
+
+/*When all matches are done( all disabled ?) Win message*/
 /*Counting clicks*/
 /*Assigning stars depending on clicks*/
