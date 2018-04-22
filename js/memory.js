456b658e46e7b9602e00f5b6ca41e0974bf6290e
Elena Nedkova <lenuza@gmail.com>
2018-04-23 01:34:36 +0300

    changed the vars in the suffle alg. to let

--- js/memory.js
+++ js/memory.js
@@ -28,14 +28,13 @@ const backSides = document.getElementsByClassName("back");
 const frontSides = document.getElementsByClassName("front");
 
 /*Shuffle cards*/
-/**
- * Randomize array element order in-place.
+/*
  * Using Durstenfeld shuffle algorithm.
  */
 function shuffleArray(array) {
-  for (var i = array.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1.1));
-    var temp = array[i];
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
     array[i] = array[j];
     array[j] = temp;
   }
